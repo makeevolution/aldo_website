a41db201f7432b5d64cbf72695a407c390c1d71c
Aldo <aldo_hasibuan@yahoo.com>
2020-09-08 17:55:31 +0200

    turn on jsonfickle

--- aldo_website_deploy/app.py
+++ aldo_website_deploy/app.py
@@ -1,22 +1,22 @@
 from flask import request, Flask, render_template, send_from_directory
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import json
-#import jsonpickle
+import jsonpickle
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
 #Create database instance for dutch learning
-# db = SQLAlchemy(app)
-# class Tests(db.Model):
-#     id=db.Column(db.Integer, primary_key=True)
-#     question=db.Column(db.String(200),unique=True)
-#     answer = db.Column(db.String(200), unique=False)
-#
-#     def __repr__(self):#Redefine what print(object) is
-#         return '{} {}'.format(self.question,self.answer)
+db = SQLAlchemy(app)
+class Tests(db.Model):
+     id=db.Column(db.Integer, primary_key=True)
+     question=db.Column(db.String(200),unique=True)
+     answer = db.Column(db.String(200), unique=False)
+
+     def __repr__(self):#Redefine what print(object) is
+         return '{} {}'.format(self.question,self.answer)
 # To create a database instance:
     #1. Go to terminal to aldo_webstie_deploy dir and fire up python
     #2. type from app import db
@@ -29,17 +29,41 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 @app.route('/dutch_training',methods=['GET','POST'])
 def dutch_training():
     data=Tests.query.all()
-    if request.method=='POST':
-        name=request.form['name']
-        return f'<h1> {name} </h1>'
-    else:
-        return render_template("dutch_training.html",data=data)
+    return render_template("dutch_training.html",data=data)
 
 @app.route('/get_data',methods=['GET','POST'])
 def get_data():
     data=Tests.query.all()
     #Here I use jsonpickle custom library in order to parse Tests class object into JSON form
     return jsonpickle.encode(data)
+
+@app.route('/add_question',methods=['GET','POST'])
+def add_question():
+    new_question=request.form['fname']
+    new_answer=request.form['lname']
+    db.create_all()
+
+     # change question and answer field to reflect what you want
+    test_1 = Tests(question=new_question, answer=new_answer)
+
+    db.session.add(test_1)
+    db.session.commit()
+    return render_template("dutch_training.html")
+
+@app.route('/delete',methods=['GET','POST'])
+def delete():
+    delete_question=request.form['deletes']
+    missing = Tests.query.filter_by(id=delete_question).first()
+    db.session.delete(missing)
+    db.session.commit()
+
+    data = Tests.query.all()
+    j=1
+    for i in data:
+        i.id=j
+        j=j+1
+    db.session.commit()
+    return render_template("dutch_training.html")
 
 @app.route("/")
 def home():
